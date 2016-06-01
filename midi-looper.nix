a9de5dcd5bae29052737e3b9da83c1da7970ce9b
rexim <reximkut@gmail.com>
2016-06-02 01:59:42 +0600

    Add rust-nightly to nix dev environment

--- midi-looper.nix
+++ midi-looper.nix
@@ -1,11 +1,18 @@
 let
   pkgs = import <nixpkgs> {};
   stdenv = pkgs.stdenv;
+  funs = pkgs.callPackage ./rust-nightly.nix { };
 in rec {
+
+  rustNightly = funs.rust {
+    date = "2016-04-09";
+    hash = "07w2fs1c4jwzsdphcr6215py7f3nid8qf920hswfn9l3fy5x9jfz";
+  };
+
   midiLooperEnv = stdenv.mkDerivation rec {
     name = "midi-looper-env";
     version = "0.0.1";
     src = ./.;
-    buildInputs = [ pkgs.rustc pkgs.cargo pkgs.portmidi ];
+    buildInputs = [ pkgs.portmidi pkgs.SDL2 rustNightly ];
   };
 }
