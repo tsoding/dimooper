75e655639d195c906ddfdac16e778b07cbcbf48e
rexim <reximkut@gmail.com>
2016-06-07 23:28:18 +0600

    Implement Updatable for Track (#19)

--- src/track.rs
+++ src/track.rs
@@ -1,15 +1,18 @@
 use pm::types::MidiEvent;
 use sdl2::render::Renderer;
+use ::updatable::Updatable;
 
 pub struct Track {
 
 }
 
-impl Track {
+impl Updatable for Track {
     fn update(&mut self, delta_time: u32) {
         unimplemented!()
     }
+}
 
+impl Track {
     fn on_midi_event(&mut self, event: &MidiEvent) {
         unimplemented!()
     }
