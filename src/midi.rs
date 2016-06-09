f025a6a5f05e5580d735bd987c328175f716d161
rexim <reximkut@gmail.com>
2016-06-09 23:15:36 +0600

    Implement Looper Rendering (#25)

--- src/midi.rs
+++ src/midi.rs
@@ -8,3 +8,7 @@ pub fn is_note_message(message: &MidiMessage) -> bool {
     let message_type = get_message_type(message);
     message_type == 0b10000000 || message_type == 0b10010000
 }
+
+pub fn get_note_key(message: &MidiMessage) -> u8 {
+    message.data1
+}
