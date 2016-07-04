bc75208e53524f558223af5c8e072e6ae2407408
rexim <reximkut@gmail.com>
2016-07-04 20:42:00 +0600

    Update dev env nix definition (#9)

--- dimooper.nix
+++ dimooper.nix
@@ -9,8 +9,8 @@ in rec {
     hash = "07674ikgc51d6kmbarb2z2izbh63jlxhc9078jk3f11v3s73n73q";
   };
 
-  midiLooperEnv = stdenv.mkDerivation rec {
-    name = "midi-looper-env";
+  dimooperEnv = stdenv.mkDerivation rec {
+    name = "dimooper-env";
     version = "0.0.1";
     src = ./.;
     buildInputs = [ pkgs.portmidi pkgs.SDL2 rustNightly ];
