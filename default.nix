54a1b3694634d217499b90e7df9abf0e7d372e18
rexim <reximkut@gmail.com>
2016-07-28 21:15:21 +0600

    Convert dimooper.nix to default.nix
    Close #109

--- default.nix
+++ default.nix
@@ -2,13 +2,11 @@ let
   pkgs = import <nixpkgs> {};
   stdenv = pkgs.stdenv;
   funs = pkgs.callPackage ./rust-nightly.nix { };
-in rec {
-
   rustNightly = funs.rust {
     date = "2016-06-12";
     hash = "07674ikgc51d6kmbarb2z2izbh63jlxhc9078jk3f11v3s73n73q";
   };
-
+in rec {
   dimooperEnv = stdenv.mkDerivation rec {
     name = "dimooper-env";
     version = "0.0.1";
