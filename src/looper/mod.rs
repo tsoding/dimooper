9b758ca98e81196df2fa347f82c41809c69154f3
rexim <reximkut@gmail.com>
2016-09-09 22:06:57 +0600

    Add FIXME(#150)

--- src/looper/mod.rs
+++ src/looper/mod.rs
@@ -214,6 +214,7 @@ impl Looper {
     pub fn update_tempo_bpm(&mut self, tempo_bpm: u32) {
         let new_measure = Measure { tempo_bpm: tempo_bpm, .. self.measure };
 
+        // FIXME(#150): Improve time cursor scaling
         self.time_cursor =
             self.measure.scale_time_cursor(&new_measure,
                                            self.amount_of_measures,
