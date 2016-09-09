8f7ed46409f913d003608df82577d31553a85c22
rexim <reximkut@gmail.com>
2016-09-09 18:46:35 +0600

    Add link to rust archive to default.nix

--- default.nix
+++ default.nix
@@ -2,6 +2,8 @@ let
   pkgs = import <nixpkgs> {};
   stdenv = pkgs.stdenv;
   funs = pkgs.callPackage ./rust-nightly.nix { };
+
+  # Archive is here: https://static.rust-lang.org/dist/index.html
   rustNightly = funs.rust {
     date = "2016-09-08";
     hash = "19m8xlhq608f8rw4dy78d9s1gsfwpj9b5pma7i3ivdkh9ap751h8";
