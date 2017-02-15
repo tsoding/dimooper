d4fbb3e61649f23e058a0a562ba42fc82f60d7d7
rexim <reximkut@gmail.com>
2017-02-16 03:05:59 +0600

    Add remaining pieces (#169)

--- src/main.rs
+++ src/main.rs
@@ -90,6 +90,12 @@ fn main() {
 
     let mut previuos_ticks = timer_subsystem.ticks();
 
+    // TODO: Implement state switcher and incorporte Port Selection
+    // State in it.
+    //
+    // State switch is basically a map StateId -> &State. When
+    // State::update() returns a different StateId the corresponding
+    // state should become the current state.
     let mut current_controller = MainLooperState::<PortMidiNoteTracker>::new(looper, bpm_popup);
 
     while running {
---
+++ src/state/port_selection_state.rs
@@ -0,0 +1,8 @@
+// TODO: Implement Port Selection State.
+//
+// It should provide the user a list of IO MIDI ports. The application
+// should remember the choice and if the list of available ports
+// didn't change since the previous session, autoselect the ports
+// selected before.
+pub struct PortSelectionState {
+}
