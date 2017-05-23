fce0a5a2cf9fe6622266c0ee415412d8cf110b30
rexim <reximkut@gmail.com>
2017-05-24 01:20:20 +0700

    Rename PortSelectionState -> PortSelectionScreen (#205)

--- src/screen/mod.rs
+++ src/screen/mod.rs
@@ -1,8 +1,8 @@
 mod state_id;
 mod state;
 mod looper_screen;
-mod port_selection_state;
+mod port_selection_screen;
 pub use self::state_id::StateId;
 pub use self::state::State;
 pub use self::looper_screen::LooperScreen;
-pub use self::port_selection_state::PortSelectionState;
+pub use self::port_selection_screen::PortSelectionScreen;
