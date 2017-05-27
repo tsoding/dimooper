a7ee0e0f74b07896e396d11e7ec762096096e676
rexim <reximkut@gmail.com>
2017-05-27 11:25:23 +0700

    Fix typo in the path::display_absolute_path doc (#202)

--- src/path.rs
+++ src/path.rs
@@ -2,8 +2,8 @@ use std;
 use std::path::{Path, PathBuf};
 
 /// Converts Path into absolute PathBuf. Should be used only for
-/// display in messages since it suppresses any errors and falls back
-/// to returning the relative path.
+/// displaying in messages since it suppresses any errors and falls
+/// back to returning the relative path.
 pub fn display_absolute_path(path: &Path) -> PathBuf {
     std::env::current_dir().map(|d| {
         d.join(path)
