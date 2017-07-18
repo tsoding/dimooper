6d697b615419bada794d390f09fc81e46157cab1
rexim <reximkut@gmail.com>
2017-07-18 16:35:17 +0700

    Setup original version of rust (#208)

--- default.nix
+++ default.nix
@@ -1,12 +1,16 @@
 let
   pkgs = import <nixpkgs> {};
   stdenv = pkgs.stdenv;
+  dimooperChannel = pkgs.rustChannelOf {
+    date = "2017-03-01";
+    channel = "nightly";
+  };
 in rec {
   dimooperEnv = stdenv.mkDerivation rec {
     name = "dimooper-env";
     version = "0.0.1";
     src = ./.;
-    buildInputs = [ pkgs.portmidi pkgs.SDL2 pkgs.SDL2_ttf pkgs.rustChannels.nightly.rust ];
-    # RUST_SRC_PATH = "${rustSrcNightly}/usr/src/rust/src/";
+    buildInputs = [ pkgs.portmidi pkgs.SDL2 pkgs.SDL2_ttf dimooperChannel.rust dimooperChannel.rust-src ];
+    RUST_SRC_PATH = "${dimooperChannel.rust-src}/lib/rustlib/src/rust/src";
   };
 }
