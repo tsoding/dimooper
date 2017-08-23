ebe0434e01d1dd65bf427df6bac26b2b2064453c
rexim <reximkut@gmail.com>
2017-08-24 02:32:26 +0700

    Implement VirtualKeyboard::from_config (#242)

--- src/ui/virtual_keyboard.rs
+++ src/ui/virtual_keyboard.rs
@@ -3,25 +3,39 @@ use sdl2::keyboard::Keycode;
 use sdl2::rect::Point;
 use sdl2::pixels::Color;
 
+use num::*;
+
 use std::collections::HashMap;
 
 use traits::*;
 use config::Config;
 
+use ui::VirtualKey;
+
 pub struct VirtualKeyboard {
-    layout: HashMap<u64, u8>
+    virtual_key: HashMap<Keycode, VirtualKey>,
 }
 
 // TODO(#242): Implement VirtualKeyboard
 impl VirtualKeyboard {
     pub fn from_config(config: &Config) -> VirtualKeyboard {
         VirtualKeyboard {
-            layout: config.keyboard_layout.clone()
+            virtual_key: ["qwertyuiop", "asdfghjkl", "zxcvbnm"]
+               .iter()
+               .flat_map(|row| {
+                   row.chars().map(|key| {
+                       let keycode = Keycode::from_name(key.to_string().as_str()).unwrap();
+                       let code = keycode.to_u64().unwrap();
+                       let midicode = config.keyboard_layout.get(&code).cloned();
+                       (keycode, VirtualKey::new(&keycode, &midicode))
+                   })
+               })
+              .collect()
         }
     }
 
     pub fn to_config(&self, config: &mut Config) {
-        config.keyboard_layout = self.layout.clone();
+
     }
 
     pub fn activate_binding(&mut self, keycode: &Keycode) {
